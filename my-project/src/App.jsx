27747d2f37820ac2fab372ae6aa19be0bc1159b0
samrth101 <samthombre91@gmail.com>
2024-07-31 04:57:24 -0700

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Hero, Workout, Generotor} from './components/Index.jsx'
+
+function App() {
+  
+  const [poison, setPoison] = useState('individual')
+  const [muscles, setMuscles] =useState([])
+  const [goal, setGoal] = useState('strength_power')
+  const [workout, setWorkout] = useState(null)
+
+  function updateWorkout() {
+    if (muscles.length < 1) {
+      return
+    }
+    let newWorkout = generateWorkout({ poison, muscles, goal })
+    setWorkout(newWorkout)
+
+    window.location.href = '#workout'
+  }
+
+
+  return (
+
+
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
+      <Hero/>
+      <Generotor
+       poison={poison}
+       setPoison={setPoison}
+       muscles={muscles}
+       setMuscles={setMuscles}
+       goal={goal}
+       setGoal={setGoal}
+       updateWorkout={updateWorkout}
+      />
+      {workout && (<Workout workout={workout} />)}
+    </main>
+  )
+}
+
+export default App
---
+++ my-project/src/components/Index.jsx
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import Generotor from  './Generator'
+import Workout from './Workout'
+import Button from "./Button";
+
+export {
+  Hero,
+  Generotor,
+  Workout,
+  Button,
+}
